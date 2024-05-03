9a795cb74ecf26ff8b51828c01c3f8555dc1ae87
Aarham Haider <105332385+AarhamH@users.noreply.github.com>
2024-05-03 15:49:14 -0700

    Updated treesitter to autoload config based on LSP

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
-  return {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
 
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {"lua", "javascript"},
-        highlight = {enabled = true},
-        indent = {enabled = true},
-      })
-    end
-  }
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
